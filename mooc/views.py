34b6d08b61de3c56678a34ae2136d521eb545125
Ayush Gupta <77526719+ayush20501@users.noreply.github.com>
2021-10-01 15:23:02 +0530

    Update views.py

--- mooc/views.py
+++ mooc/views.py
@@ -14,7 +14,7 @@ def home(request):
         return redirect("/")
     else:
         data = Image.objects.all()
-        return render(request, 'home.html',{'data':data})
+        return render(request, 'home.html')
 
 
 def confess(request):
