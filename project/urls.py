83c889ad215882c4324544ee0f136b353644255b
Ayush Gupta <77526719+ayush20501@users.noreply.github.com>
2022-07-14 11:50:42 +0530

    Update urls.py

--- project/urls.py
+++ project/urls.py
@@ -19,10 +19,10 @@ from mooc import views
 from django.conf.urls import include, url
 from django.conf.urls.static import static
 from django.conf.urls import url
-from django.conf import settings
+
 
 
 urlpatterns = [
     url('admin/', admin.site.urls),
     path('',include('mooc.urls'))
-]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+]
