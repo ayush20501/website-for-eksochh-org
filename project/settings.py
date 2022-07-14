ecc851e4106ba82fafeb4f48d6c7c8dabc17e22a
Ayush Gupta <77526719+ayush20501@users.noreply.github.com>
2022-07-14 11:50:05 +0530

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -39,8 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'cloudinary_storage',
-    'cloudinary',
 ]
 
 MIDDLEWARE = [
@@ -124,17 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR / "static")
 
-]
-MEDIA_ROOT = BASE_DIR/ 'media'
-CLOUDINARY_STORAGE={
-    'CLOUD_NAME':'df4jgeuod',
-    'API_KEY':'733437518658272',
-    'API_SECRET':'V9FaRN3JxP5S1xdHbq8WSlEopyY'
-}
-DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
 django_heroku.settings(locals())
